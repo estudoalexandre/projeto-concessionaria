1652179d78c0e9288178f65d1e8cf387e5311f85
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-12-20 19:11:37 -0300

    Inicializando View sem DB por enquanto

--- src/br/com/fecaf/view/VeiculoView.java
+++ src/br/com/fecaf/view/VeiculoView.java
@@ -1,5 +1,32 @@
 package br.com.fecaf.view;
 
+import java.util.List;
+
+import br.com.fecaf.model.VeiculoModel;
+
 public class VeiculoView {
-    
+    public void exibirVeiculos(List<VeiculoModel> veiculos) {
+        for (VeiculoModel veiculo : veiculos) {
+            System.out.println("Marca: " + veiculo.getMarca());
+            System.out.println("Modelo: " + veiculo.getModelo());
+            System.out.println("Tipo de Veículo: " + veiculo.getTipoVeiculo());
+            System.out.println("Ano de Fabricação: " + veiculo.getAnoFabricacao());
+            System.out.println("Preço: " + veiculo.getPreco());
+            System.out.println("Status de Disponibilidade: " + veiculo.getStatusDisponibilidade());
+            System.out.println("Cor: " + veiculo.getCor());
+            System.out.println("Quilometragem: " + veiculo.getQuilometragem());
+            System.out.println("Tipo de Combustível: " + veiculo.getTipoCombustivel());
+            System.out.println("Número de Portas: " + veiculo.getNumeroPortas());
+            System.out.println("Tipo de Câmbio: " + veiculo.getTipoCambio());
+            System.out.println();
+        }
+    }
+
+    public void mensagemSucesso(String veiculo) {
+        System.out.println("Veiculo" + veiculo + "cadastrado com sucesso!");
+    }
+
+    public void mensagemErro(String erro){
+        System.out.println("Erro ao cadastrar veiculo: " + erro);
+    }
 }
